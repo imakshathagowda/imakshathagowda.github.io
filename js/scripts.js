46ed20bc1046f90a3175b94d33ea63107f912109
AKSHATHA K <imakshathagowda@gmail.com>
2025-04-17 21:06:30 +0530

    Delete js directory

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-
-function showSection(id) {
-  document.querySelectorAll('.section').forEach(s => s.classList.remove('active'));
-  document.getElementById(id).classList.add('active');
-}
-
-function toggleTheme() {
-  document.body.classList.toggle('dark-mode');
-}
