ca7af803457f91756f0b645b5eaac8255b892631
AKSHATHA K <imakshathagowda@gmail.com>
2025-04-17 22:38:21 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+function toggleTheme() {
+  document.body.classList.toggle('dark');
+}
+function showSection(id) {
+  document.querySelectorAll('.section').forEach(s => s.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
+}
